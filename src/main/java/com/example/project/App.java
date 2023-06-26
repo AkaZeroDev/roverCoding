1b6c8783bd429cde896118f49210818a0d27f9a7
Counil Laurent <counil.laurent@gmail.com>
2023-06-26 23:28:18 +0200

    add input pom and main

---
+++ src/main/java/com/example/project/App.java
@@ -0,0 +1,25 @@
+package com.example.project;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // command line parameter
+        if(args.length != 1) {
+            System.err.println("Invalid command line, exactly one argument required");
+            System.exit(1);
+        }
+        
+        Mission m;
+        try {
+            Path filePath = Path.of(args[0]);
+            String content = Files.readString(filePath);
+            m = new Mission(content);
+        } catch (FileNotFoundException e) {
+            String errorContent = "1 1\n1 1 N\nNOT VALID\n1 1 N\nNOT VALID\n";
+            m = new Mission(errorContent);
+        }
+    }
+}
